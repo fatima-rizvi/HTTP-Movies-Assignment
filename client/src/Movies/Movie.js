daf6b0ea99d0e286587bf4eb492e6b7989669849
Fatima Rizvi <fatimarzv23@gmail.com>
2020-10-14 17:16:59 -0500

    Delete functionality working!

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -3,11 +3,14 @@ import axios from "axios";
 import { useParams, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
-function Movie({ addToSavedList }) {
+function Movie(props) {
+  const { addToSavedList, setMovieList, movieList } = props
   const [movie, setMovie] = useState(null);
   const params = useParams();
+  
 
   const fetchMovie = (id) => {
+    //console.log('params: ', params)
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
       .then((res) => setMovie(res.data))
@@ -24,6 +27,40 @@ function Movie({ addToSavedList }) {
     push(`/update-movie/${id}`)
   }
 
+  const useUpdateMovie = () => {
+    updateMovie(params.id)
+  }
+
+  const deleteMovie = (id) => {
+    console.log("delete id: ", id)
+    axios.delete(`http://localhost:5000/api/movies/${id}`)
+      .then((res) => {
+        console.log("delete res: ", res)
+        const movies = movieList.filter( movie => 
+          movie.id !== res.data
+        )
+        console.log('new movies', movies)
+        setMovieList(movies)
+        console.log('set movies: ', movieList)
+        push('/')
+      })
+      .catch((err) => {
+        console.log("Delete error: ", err)
+      })
+  }
+
+  // .then( res => {
+  //   movieList = movies.filter( movie => 
+  //     movie.id !== res.data
+  //   )
+  //   setMovieList(movies)
+  //   history.push('/')
+  //   })
+
+  const useDeleteMovie = () => {
+    deleteMovie(params.id)
+  }
+
   useEffect(() => {
     fetchMovie(params.id);
   }, [params.id]);
@@ -36,8 +73,11 @@ function Movie({ addToSavedList }) {
     <div className="save-wrapper">
       <MovieCard movie={movie} />
 
-      <button className="update-button" onClick={updateMovie}>
+      <button className="update-button" onClick={useUpdateMovie}>
         Update
+      </button>
+      <button className="delete-button" onClick={useDeleteMovie}>
+        Delete
       </button>
       <div className="save-button" onClick={saveMovie}>
         Save
